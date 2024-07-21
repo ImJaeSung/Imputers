c657d4129a15751d004b0477d918f8c12c0e4aa0
통계데이터사이언스학과/임재성 <lim8722@office.uos.ac.kr>
2024-07-21 22:36:39 +0900

    completed

--- VAEAC/imputer.py
+++ VAEAC/imputer.py
@@ -49,30 +49,6 @@ run = wandb.init(
     # entity=entity, 
     tags=["imputation"], # put tags of this python project
 )
-#%%
-def compute_normalization(data, one_hot_max_sizes):
-    """
-    Compute the normalization parameters (i. e. mean to subtract and std
-    to divide by) for each feature of the dataset.
-    For categorical features mean is zero and std is one.
-    i-th feature is denoted to be categorical if one_hot_max_sizes[i] >= 2.
-    Returns two vectors: means and stds.
-    """
-    norm_vector_mean = torch.zeros(len(one_hot_max_sizes))
-    norm_vector_std = torch.ones(len(one_hot_max_sizes))
-    for i, size in enumerate(one_hot_max_sizes):
-        if size >= 2:
-            continue
-        v = data[:, i]
-        v = v[~torch.isnan(v)]
-        vmin, vmax = v.min(), v.max()
-        vmean = v.mean()
-        vstd = v.std()
-        norm_vector_mean[i] = vmean
-        norm_vector_std[i] = vstd
-    return norm_vector_mean, norm_vector_std 
-
-
 # %%
 def str2bool(v):
     if isinstance(v, bool):
