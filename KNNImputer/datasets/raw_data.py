5f5394a9712cc3e63266420c7fdff1c2fd6b7d5b
ImJaesung <wotjd1410@gmail.com>
2024-08-02 14:15:38 +0900

    dataset: high
    dim

--- KNNImputer/datasets/raw_data.py
+++ KNNImputer/datasets/raw_data.py
@@ -562,4 +562,43 @@ def load_raw_data(config):
          
         ClfTarget = 'class14'
 
+    ########################################################
+    #--------------------High-Dimensional-----------------=#
+    ########################################################
+    elif config["dataset"] == "madelon":
+        data, _ = arff.loadarff('./data/phpfLuQE4.arff') # output : data, meta
+        data = pd.DataFrame(data)
+        
+        assert data.isna().sum().sum() == 0
+
+        for column in data.select_dtypes([object]).columns:
+            data[column] = data[column].str.decode('utf-8') # object decoding
+        
+        continuous = [i for i in range(1, 501)]
+        continuous_features = [f"V{x}" for x in continuous]
+        
+        categorical_features = ["Class"]
+        integer_features = continuous_features
+         
+        ClfTarget = 'Class'
+
+
+    elif config["dataset"] == "bioresponse":
+        data, _ = arff.loadarff('./data/phpSSK7iA.arff') # output : data, meta
+        data = pd.DataFrame(data)
+        
+        assert data.isna().sum().sum() == 0
+
+        for column in data.select_dtypes([object]).columns:
+            data[column] = data[column].str.decode('utf-8') # object decoding
+        
+        continuous = [i for i in range(1, 1777)]
+        continuous_features = [f"D{x}" for x in continuous]
+        
+        categorical_features = ["target"]
+        integer_features = []
+         
+        ClfTarget = 'target'
+
+
     return data, continuous_features, categorical_features, integer_features, ClfTarget
